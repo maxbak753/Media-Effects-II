dc14656bcd88895dedd47aff285615daee1637fd
maxbak753 <114166327+maxbak753@users.noreply.github.com>
2024-11-08 13:16:49 -0500

    added XY gravity, friction, air resistance, etc (particle simulator)

--- C++_Stuff/Particle_Simulator/Particles_1/particles_1.cpp
+++ C++_Stuff/Particle_Simulator/Particles_1/particles_1.cpp
@@ -18,9 +18,41 @@ using namespace std;
 
 // Functions
 
+// // Classes
+// class particle {
+//   public:
+//     float ax = 0.0; // acceleration (gravity) in x-direction
+//     float ay = 0.3; // acceleration (gravity) in y-direction
+//     float tx = 0; // time for x-gravity
+//     float ty = 0; // time for y-gravity
+//     bool position_invalid; // true if the position is invalid (out-of-bounds)
+//     float bounce_loss = 0.9; // bounce loss (% of velocity to keep after bounce)
+//     float air_res = 0.9999; // air resistance (% of velocity kept throughout time)
+//     float fric = 0.99; // friction (% of velocity kept while touching the walls)
+//     float dp[2]; // Δp = change in position
+
+//     particle(string choice) {
+//         if (choice == "frictionless") {
+//             bounce_loss = 0;
+//             air_res = 0;
+//             fric = 0;
+//         }
+//     }
+//     particle(float gx, float gy) {
+//         ax = gx;
+//         ay = gy;
+//     }
+// };
+
 // Main
 int main(int argc, char** argv ) {
-    int dims[2] = {50, 400};
+    cout << " /--- - - -      - - - ---\\" << endl;
+    cout << "{*}~ PARTICLE SIMULATOR ~{*}" << endl;
+    cout << " \\--- - - -      - - - ---/" << endl;
+    cout << "" << endl;
+    cout << "  hold ESC or 'q' to exit   " << endl;
+
+    int dims[2] = {50, 100};
     cv::Mat field = cv::Mat::zeros(dims[0], dims[1], CV_8U);
 
     // Display the matrix
@@ -28,68 +60,126 @@ int main(int argc, char** argv ) {
 
     float position[2] = {static_cast<float>(dims[0]/2), static_cast<float>(dims[1]/2)};
     float proposed_position[2] = {0,0};
-    float direction[2] = {2,3};
+    float direction[2] = {3,1};
     int disp_count = 0;
     // bool proposed_position_invalid;
 
     int v_i = static_cast<int>(max(direction[0], direction[1])); // direction interval for a whole number as smallest x-y component
     float v[2] = {direction[0] / v_i, direction[1] / v_i}; // normalized velocity vector where the largest interval component is 1 unit
+    float v_scale = 5;
+    v[0] *= v_scale; v[1] *= v_scale; // scale velocity (spped-up / slow-down)
 
-    // imshow("Field", field);
-    cv::namedWindow("Field", cv::WINDOW_NORMAL);
-    cv::resizeWindow("Field", 4*dims[1], 4*dims[0]);
+    // imshow("Particle Simulation", field);
+    cv::namedWindow("Particle Simulation", cv::WINDOW_NORMAL);
+    cv::resizeWindow("Particle Simulation", 6*dims[1], 6*dims[0]);
+    int display_interval = 7; // after how many iterations does a new frame of the simulation appear
 
-    for (int i=0; i < 500; ++i) {
+
+    int secs_timeout = 60; // seconds after which the window closes
+    // Record the start time
+    auto start = std::chrono::steady_clock::now();
+
+    
+
+    float ax = 0.000; // acceleration (gravity) in x-direction
+    float ay = 0.003; // acceleration (gravity) in y-direction
+    float tx = 0;
+    float ty = 0;
+    bool position_invalid;
+    int pos_inv_count = 0;
+
+    float bounce_loss = 0.9; // bounce loss (% of velocity to keep after bounce)
+    float air_res = 0.9999; // air resistance (% of velocity kept throughout time)
+    float fric = 0.99; // friction (% of velocity kept while touching the walls)
+
+    float dp[2];
+
+    int iw = 0;
+
+    // wall_collisions(direction, velocity, secs_timeout)
+
+    while (true) {
+        iw += 1;
         // field.at<uchar>(position[0], position[1]) = 0; // clear old position brightness
         // fade brightness
         field *= 0.9;
 
-        proposed_position[0] = position[0] + v[0];
-        proposed_position[1] = position[1] + v[1];
+        // calculate new proposed position
+        dp[0] = v[0] + (ay*ty); dp[1] = v[1] + (ax*tx);
+        proposed_position[0] = position[0] + dp[0];
+        proposed_position[1] = position[1] + dp[1];
 
         // If it will be out of bounds, reverse the direction (bounce)
+        position_invalid = 1;
+        while (position_invalid) {
+            bool bound_flag[2] = {((proposed_position[0] < 0) || (proposed_position[0] >= dims[0])), ((proposed_position[1] < 0) || (proposed_position[1] >= dims[1]))};
+            pos_inv_count += 1;
 
-        bool bound_flag[2] = {((proposed_position[0] <= 0) || (proposed_position[0] >= dims[0])), ((proposed_position[1] <= 0) || (proposed_position[1] >= dims[1]))};
+            if (bound_flag[0] && bound_flag[1]) {
+                tx = 0; ty = 0; // reset time for gravity (x & y)
+                v[0] *= -bounce_loss; v[1] *= -bounce_loss;
+            }
+            else if (bound_flag[0]) {
+                ty = 0;
+                v[0] *= -bounce_loss;
+            }
+            else if (bound_flag[1]) {
+                tx = 0;
+                v[1] *= -bounce_loss;
+            }
+            else {
+                position_invalid = 0;
+                pos_inv_count = 0;
+                tx += 1;
+                ty += 1;
+                break;
+            }
 
-        if (bound_flag[0] && bound_flag[1]) {
-            v[0] *= -1; v[1] *= -1;
-            proposed_position[0] = position[0] + v[0];
-            proposed_position[1] = position[1] + v[1];
-            // cout << "BOUND FLAG: " << bound_flag[0] << " " << bound_flag[1] << endl;
-            // cout << "Corner!" << endl;
+            // calculate new proposed position
+            dp[0] = v[0] + (ay*ty); dp[1] = v[1] + (ax*tx);
+            proposed_position[0] = position[0] + dp[0];
+            proposed_position[1] = position[1] + dp[1];
         }
-        else if (bound_flag[0]) {
-            v[0] *= -1;
-            proposed_position[0] = position[0] + v[0];
-            proposed_position[1] = proposed_position[1];
-            // cout << "BOUND FLAG: " << bound_flag[0] << " " << bound_flag[1] << endl;
-            // cout << "0 Bound" << endl;
-        }
-        else if (bound_flag[1]) {
-            v[1] *= -1;
-            proposed_position[0] = proposed_position[0];
-            proposed_position[1] = position[1] + v[1];
-            // cout << "BOUND FLAG: " << bound_flag[0] << " " << bound_flag[1] << endl;
-            // cout << "1 Bound" << endl;
-        }
-
+        
         position[0] = proposed_position[0]; position[1] = proposed_position[1];
-        // cout << ".";
 
-        field.at<uchar>(std::round(position[0]), std::round(position[1])) = 255; // make new position bright
+        field.at<uchar>(std::floor(position[0]), std::floor(position[1])) = 255; // make new position bright (floor for sitting on bottom)
 
-        if (disp_count >= 1) {
-            cv::imshow("Field", field);
+        if (disp_count >= display_interval) {
+            cv::imshow("Particle Simulation", field);
             cv::waitKey(1);
             disp_count = 0;
+
+            // cout << "Display" << endl;
+
+            // Exit loop if 'q' or ESC is pressed
+            if ((cv::waitKey(1) == 'q') || (cv::waitKey(1) == 27)) {
+                break;
+            }
+
+
+            // Calculate elapsed time
+            auto now = std::chrono::steady_clock::now();
+            auto elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(now - start).count();
+
+            // Break if elapsed time is too long
+            if (elapsed_seconds >= secs_timeout) {
+                std::cout << secs_timeout << " seconds have elapsed: closing window" << std::endl;
+                break;
+            }
         }
         disp_count += 1;
+
+        v[0] *= air_res; v[1] *= air_res; // apply air resistance
+
+        if ((round(position[0]) == 0) || (round(position[0]) == dims[0]) || (round(position[1]) == 0) || (round(position[1]) == dims[1])) {
+            v[0] *= fric; v[1] *= fric; // friction
+        }
+
     }
 
-    cout << "{*}~ THE END ~{*}" << endl;
+    cout << "     {*}~ THE END ~{*}" << endl;
 
-    // Keep the final result displayed until a key is pressed
-    cv::waitKey(0);
     return 0;
     
 }
